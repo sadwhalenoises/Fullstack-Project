4a9ef5dba85ebfde95522f0a34ae7ca0a762f94e
sadwhalenoises <carlton.shinall@gmail.com>
2022-08-29 15:02:03 -0500

    Added component and img folder

--- src/main/fullstack/src/App.js
+++ src/main/fullstack/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import Homepage from "./components/Homepage";
+import Navigation from "./components/Navigation";
 
+const TestData = () =>{
+
+  const fetchData = () => {
+    axios.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Doublelift?api_key=RGAPI-e6a7bc54-dc02-4783-8658-71bdac934ee8").then(res => {
+      console.log(res);
+    });
+  };
+
+  useEffect(() =>{
+    fetchData();
+  },[]);
+  
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      
     </div>
   );
 }
--- src/main/fullstack/src/components/Homepage.js
+++ src/main/fullstack/src/components/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Homepage() {
   return (
-    
+    <div>Hello</div>
   )
 }
 
